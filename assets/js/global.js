7e06bfa03ccd47a8e2728cd179759c4dfa1c554e
GOUSPY Jon <97737104+Gerid0@users.noreply.github.com>
2024-01-31 15:20:31 +0000

    Patch nav bar

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,10 +1,13 @@
 const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
 
-const token = 2;
+const token = 0;
 
 if (token == 1) {
     addCSS(".teacher { display: initial; }")
 }
 else if (token == 2) {
     addCSS(".devlopper { display: initial; }")
+}
+else {
+    addCSS(".devlopper { display: none; }")
 }
