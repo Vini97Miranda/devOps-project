71c5e70329a7aa624a29a2d441832d08abe1a170
Jon Gouspy <jon.gouspy@epita.fr>
2024-02-07 15:33:20 +0000

    Main page

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,16 +1,20 @@
 const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
 
-const token = 0;
+const token = 2;
 
 if (token == 1) {
-    addCSS(".teacher { display: block; }")
+    addCSS(".teacher { display: block; }");
+    addCSS(".topnav.responsive a.teacher { display: block; }");
 }
 else if (token == 2) {
-    addCSS(".developer { display: block; }")
+    addCSS(".developer { display: block; }");
+    addCSS(".topnav.responsive a.developer { display: block; }");
 }
 else {
     addCSS(".developer { display: none; }")
 }
+
+console.log(token);
 
 function hamburger() {
     var x = document.getElementById("myTopnav");
