898224933d112e4f3bb71ee467832237de155b6d
Jon Gouspy <97737104+Gerid0@users.noreply.github.com>
2024-03-27 15:20:25 +0000

    Gollum part du projet

--- BankingApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BankingApp.MvcApplication" Language="C#" %>
--- BankingApp/Scripts/logout-btn.js
+++
@@ -1,4 +0,0 @@
-﻿//jon gouspy 74538
-document.getElementById("logout-btn").addEventListener("click", function () {
-    window.scrollTo(0, document.body.scrollHeight);
-});
