c6539d3ff3e167e86061d06d0c750b844114262c
Jon Gouspy <97737104+Gerid0@users.noreply.github.com>
2024-03-27 13:43:53 +0000

    Model + LoginController

---
+++ DevOps-Project/Controllers/LoginController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DevOps_Project.Models;
+
+namespace DevOps_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        private DBModels db = new DBModels();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            var account = db.Account.Include(a => a.Cohort);
+            return View(account.ToList());
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Account.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: Login/Create
+        public ActionResult Create()
+        {
+            ViewBag.CohortId = new SelectList(db.Cohort, "Id", "CohortName");
+            return View();
+        }
+
+        // POST: Login/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FisrtName,LastName,Email,Password,AccountType,CohortId")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Account.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CohortId = new SelectList(db.Cohort, "Id", "CohortName", account.CohortId);
+            return View(account);
+        }
+
+        // GET: Login/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Account.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CohortId = new SelectList(db.Cohort, "Id", "CohortName", account.CohortId);
+            return View(account);
+        }
+
+        // POST: Login/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FisrtName,LastName,Email,Password,AccountType,CohortId")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CohortId = new SelectList(db.Cohort, "Id", "CohortName", account.CohortId);
+            return View(account);
+        }
+
+        // GET: Login/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Account.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Login/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Account account = db.Account.Find(id);
+            db.Account.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
